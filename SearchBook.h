770527f0b93ad0efac38b8c05be99a9055460546
arbor504 <152618839+arbor504@users.noreply.github.com>
2023-12-03 18:02:28 +0800

    Add files via upload

--- SearchBook.h
+++ SearchBook.h
@@ -4,14 +4,11 @@
 #include<vector>
 #include "myLibType.h"
 
-class SearchBook
-{
-    public:
-        SearchBook();
-        virtual ~SearchBook();
-        vector<Book> findbook(std::string name,std::string author,std::string publisher,std::string categoryNumber);
+namespace Lib {
+
+        std::vector<Book> findbook(std::string name,std::string author,std::string publisher,std::string categoryNumber);
         bool comparison(std::string sentence1,std::string sentence2);
         bool Judgement(unsigned char c);
-};
+}
 
 #endif // SEARCHBOOK_H
