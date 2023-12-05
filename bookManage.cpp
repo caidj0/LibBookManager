3dee6bc87a80e557517d03784f4b5c5b333ff251
Ozileid <152495000+Ozileid@users.noreply.github.com>
2023-12-05 20:22:18 +0800

    Add files via upload

---
+++ bookManage.cpp
@@ -0,0 +1,14 @@
+#include"bookManage.h"
+#include "SearchBook.h"
+#include <map>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include "myLibType.h"
+using namespace std;
+extern vector<Book> books;
+void Lib::disp(){
+    
+};
